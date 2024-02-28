af9bb611ef0e646919b1da565ca392cb9775e44a
eduardferre <eduardferresanchez@gmail.com>
2024-02-28 13:59:36 +0100

    map

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import EventFormPage from './pages/EventFormPage/EventFormPage';
-import EventInfo from './pages/EventInfo/EventInfo';
-import NotFound from './pages/NotFound/NotFound';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import EventFormPage from './pages/EventFormPage/EventFormPage'
+import EventInfo from './pages/EventInfo/EventInfo'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
-  
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
